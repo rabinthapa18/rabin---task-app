37fd73729b767289acb72b9c8b5db411f49c7d7e
rabinthapa18 <rabinson.thapa.18@gmail.com>
2021-05-11 14:52:57 +0530

    moongose error handling

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,5 +2,11 @@ const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
-    useCreateIndex: true
-})
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log('MongoDB successfully connected. Database URL:', process.env.MONGODB_URL);
+}).catch(error => {
+    console.log('Error establishing MongoDB connection. Error:', error.message);
+});
--- src/routers/user.js
+++ src/routers/user.js
@@ -21,7 +21,7 @@ router.post('/users/signup', async (req, res) => {
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
     } catch (e) {
-        res.status(400).send()
+        res.status(400).send(e)
     }
 })
 
